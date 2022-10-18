4101405a57d37842b7fe91c80063d911131024a5
wkg7vb <90648776+wkg7vb@users.noreply.github.com>
2022-10-18 14:54:17 -0500

    commented code

--- Assignment 2/CS303 Assignment 2/Assignment2Main.cpp
+++ Assignment 2/CS303 Assignment 2/Assignment2Main.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 int main()
 {
-	Professional Worker1 = Professional(10000);
-	Nonprofessional Worker2 = Nonprofessional(45, 25, 4);
+	Professional Worker1 = Professional(10000); // this can be edited, it is the monthly salary of the professional employee
+	Nonprofessional Worker2 = Nonprofessional(45, 25, 4); // this can be edited, it is the hours, wage, and choice in contribution of the nonprofessional employee
 
+	//prints the data from each employee so that you can see the changes in parameters result in different member data.
 	Worker1.printData();
 	Worker2.printData();
 
--- Assignment 2/CS303 Assignment 2/Employee.h
+++ Assignment 2/CS303 Assignment 2/Employee.h
@@ -4,53 +4,63 @@
 #include <iostream>
 using namespace std;
 
-
+//creation of abtsratc class employee
 class Employee
 {
-	protected:
+	protected: // want derived classes to be able to access member data
 		double w_pay;
 		int v_days;
-		double hc_contributions;
-	public:
+		int hc_contributions;
+	public: //pure virtual functions to be overriden in derived classes
 		virtual double weeklyPay() = 0;
 		virtual int vacationDays() = 0;
 		virtual int HCContributions() = 0;
+
+		//setters and getters, although not used
+		void setPay(double pay) { w_pay = pay; };
+		double getPay() { return w_pay; };
+		void setDays(double days) { v_days = days; };
+		int getDays() { return v_days; };
+		void setHCC(double hcc) { hc_contributions = hcc; };
+		int getHCC() { return hc_contributions; };
 };
 
+//creation of derived class professional
 class Professional : public Employee 
 {
-	private:
+	private: //member data exclusive to this derived class
 		double m_salary;
 
 	public:
-		Professional(double msal) { m_salary = msal; };
+		Professional(double msal) { m_salary = msal; }; //constructor
 
-		double weeklyPay() { w_pay = (m_salary / 30) * 7; return w_pay; };
-		int vacationDays() { v_days = 15; return v_days; };
-		int HCContributions() { hc_contributions = 5; return hc_contributions; };
+		double weeklyPay() { w_pay = (m_salary / 30) * 7; return w_pay; }; //calcualtions based on 30 day month and 7 day week
+		int vacationDays() { v_days = 15; return v_days; }; //always 15 days for this class
+		int HCContributions() { hc_contributions = 5; return hc_contributions; }; //always 5% for this class
 
-		void printData()
+		void printData() //prints the relevant data using cout
 		{
 			cout << " This worker is set to make " << weeklyPay() << " per week," << endl
 				<< " get " << vacationDays() << " days of vacation per year," << endl
 				<< " and is expected to contribute " << HCContributions() << " percent of their wages to their health insurance." << endl;
 		}
 		
+		//getter and setters for private data, even though not used.
 		void setSalary(double msal) { m_salary = msal; };
 		double getSalary() { return m_salary; };
 };
 
 class Nonprofessional : public Employee 
 {
-	private:
+	private: //member data exclusive to this derived class
 		double w_hours;
 		double h_wage;
 		int hc_choice;
 
 	public:
-		Nonprofessional(double hours, double wage, double choice) { w_hours = hours; h_wage = wage; hc_choice = choice; };
+		Nonprofessional(double hours, double wage, double choice) { w_hours = hours; h_wage = wage; hc_choice = choice; }; //constructor
 
-		double weeklyPay()
+		double weeklyPay() //calculates pay as hours * wage, ignoring negative hours, and calculating overtime
 		{
 			if (w_hours < 0)
 			{
@@ -66,7 +76,7 @@ class Nonprofessional : public Employee
 			}
 			return w_pay;
 		};
-		int vacationDays()
+		int vacationDays() // casts hours as an int to do integer division, for every 10 hours worked per week, you get 2 additional vacation days with a starting amount of 2
 		{
 			switch (int(w_hours) / 10)
 			{
@@ -100,22 +110,24 @@ class Nonprofessional : public Employee
 					v_days = 12;
 					break;
 				}
-				default:
+				default: // this case should never be triggered but if it is, someone working less than 0 or more than 60 hours will get no vacation, they either don't work enough or are too vital
 				{
+					v_days = 0;
 					break;
 				}
 			}
 			return v_days;
 		};
-		int HCContributions() { hc_contributions = hc_choice; return hc_contributions; };
+		int HCContributions() { hc_contributions = hc_choice; return hc_contributions; }; //the employee can choose between 0 and 5 percent.
 
-		void printData()
+		void printData() //prints the relevant data using cout
 		{
 			cout << " This worker is set to make " << weeklyPay() << " per week," << endl
 				<< " get " << vacationDays() << " days of vacation per year," << endl
 				<< " and is expected to contribute " << HCContributions() << " percent of their wages to their health insurance." << endl;
 		}
 
+		//setters and getters, although not used.
 		void set_hours(double hrs) { w_hours = hrs; }
 		double get_hours() { return w_hours; }
 		void set_wage(double wage) { h_wage = wage; }
